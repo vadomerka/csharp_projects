7a3db8784c0670e7ebd9987a27ae612c4a28e174
vadomerka <pradomiri@gmail.com>
2025-06-11 16:26:15 +0300

    HseShop PaymentsService

---
+++ HW_CPS_HseShop/PaymentsService.Web/Controllers/PaymentsServiceController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaymentsService.Entities.Common;
+using PaymentsService.Infrastructure;
+using PaymentsService.Infrastructure.Facades;
+
+namespace FilesStoringService.Controllers
+{
+    [ApiController]
+    [Route("/api")]
+    public class PaymentsServiceController : ControllerBase
+    {
+        private readonly AccountDBContext _context;
+
+        public PaymentsServiceController(AccountDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("/accounts")]
+        public ActionResult<IEnumerable<Account>> Get()
+        {
+            var ur = new AccountFacade(_context);
+            var accs = ur.GetAll();
+            return Ok(accs);
+        }
+
+        [HttpGet("/account/{id}")]
+        public ActionResult<Account> Get(Guid id)
+        {
+            var ur = new AccountFacade(_context);
+            try
+            {
+                var res = ur.GetAccount(id);
+                return Ok(res);
+            }
+            catch (FileNotFoundException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("/account/{id}")]
+        public IActionResult AddMoney(Guid id, Decimal money)
+        {
+            var facade = new AccountFacade(_context);
+            try
+            {
+                var res = facade.AddToAccount(id, money);
+
+                return Ok(new { res });
+            }
+            catch (ArgumentNullException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("/account")]
+        public async Task<IActionResult> Post([FromForm] AccountDTO dto)
+        {
+            var facade = new AccountFacade(_context);
+            try
+            {
+                var res = facade.AddAccount(dto);
+
+                return Ok(new { res.Id });
+            }
+            catch (ArgumentNullException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ HW_CPS_HseShop/PaymentsService.Web/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentsService.Infrastructure;
+using PaymentsService.Infrastructure.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? "Host=localhost;Database=userfiles_db;Username=postgres;Password=postgres";
+builder.Services.AddDbContext<AccountDBContext>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.Services.AddScoped<AccountRepository>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AccountDBContext>();
+    //dbContext.Database.EnsureDeleted();
+    dbContext.Database.EnsureCreated();
+    dbContext.SaveChanges();
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
